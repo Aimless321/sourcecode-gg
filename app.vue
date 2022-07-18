8e662f26d258f66d4f3d8033cab4e42828601786
Wesley Bijleveld <6079485+Aimless321@users.noreply.github.com>
2022-07-18 13:59:23 +0200

    add title

--- app.vue
+++ app.vue
@@ -8,4 +8,12 @@
 
 <script setup>
 import 'assets/css/main.css'
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Sourcecode\'s brain dump` : 'Sourcecode\'s brain dump';
+  },
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+})
 </script>
