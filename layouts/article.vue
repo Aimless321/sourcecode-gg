b720b672c94d938f03204640a32c8ec599bbbf57
Wesley Bijleveld <6079485+Aimless321@users.noreply.github.com>
2022-08-03 22:47:50 +0200

    add hash scroll behaviour

--- layouts/article.vue
+++ layouts/article.vue
@@ -6,3 +6,27 @@
     </article>
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+
+const findEl = async (hash, x) => {
+  return document.querySelector(hash) ||
+      new Promise((resolve, reject) => {
+        if (x > 50) {
+          return resolve()
+        }
+        setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
+      })
+}
+
+if(route.hash) {
+  const el = await findEl(route.hash);
+
+  if ('scrollBehavior' in document.documentElement.style) {
+    window.scrollTo({ top: el.offsetTop })
+  } else {
+    window.scrollTo(0, el.offsetTop)
+  }
+}
+</script>
